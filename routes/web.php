b38dc424c64a4db2b4c70c90a77f89365e248c80
bimoan09 <bimoan09@gmail.com>
2018-07-09 14:36:58 +0700

    route add

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->get('/boards', 'BoardController@index');
+$router->post('/boards', 'BoardController@store');
+
